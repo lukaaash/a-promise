9206832b7f0f81ba118d18e7ad9d1ef3d6b00306
Steel Brain <aneesiqbalbhatti@gmail.com>
2015-06-08 20:31:20 -0700

    :new: Make constructor work

--- Source/Promise.js
+++ Source/Promise.js
@@ -3,9 +3,16 @@
 // @Compiler-Output "../Dist/Promise.js"
 // @Compiler-Transpile "true"
 "use strict";
+// State:Pending = 0
+// State:Success = 1
+// State:Failure = 2
 class Promise{
   constructor(Callback){
-
+    this.State = 0;
+    let Me = this;
+    setTimeout(function(){
+      Callback(function(Result){Me.resolve(Result)}, function(Result){Me.reject(Result)});
+    }, 0);
   }
   resolve(){
 
