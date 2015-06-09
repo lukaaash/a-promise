639955d5a09eb8634120fed9444111525bf8a2a7
Steel Brain <aneesiqbalbhatti@gmail.com>
2015-06-08 20:28:34 -0700

    :new: Add a basic struct

--- Source/Promise.js
+++ Source/Promise.js
@@ -3,3 +3,26 @@
 // @Compiler-Output "../Dist/Promise.js"
 // @Compiler-Transpile "true"
 "use strict";
+class Promise{
+  constructor(Callback){
+
+  }
+  resolve(){
+
+  }
+  reject(){
+
+  }
+  static defer(){
+
+  }
+  static all(){
+
+  }
+  static resolve(){
+
+  }
+  static reject(){
+
+  }
+}
